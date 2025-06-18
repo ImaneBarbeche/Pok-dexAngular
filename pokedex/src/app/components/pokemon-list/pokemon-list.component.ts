7eac363cae1a123b2c0530a162e9332af52b3b03
Imane <barbecheimane@gmail.com>
2025-06-18 16:28:37 +0200

    début du code pour récupérer les pokémons avec l'api + petit style scss

--- pokedex/src/app/components/pokemon-list/pokemon-list.component.ts
+++ pokedex/src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { PokemonService } from '../../services/pokemon.service';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.scss',
 })
-export class PokemonListComponent {}
+
+export class PokemonListComponent {
+  public pokemonService = inject(PokemonService);
+  pokemons$ = this.pokemonService.getPokemons()
+}
--- pokedex/src/app/services/pokemon.service.ts
+++ pokedex/src/app/services/pokemon.service.ts
@@ -1,12 +1,90 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+// interface Pokemon {
+//   name: string;
+//   id: number;
+//   imageUrl?: string;
+//   type: {
+//     name: string,
+//     url: string,
+//   };
+//   poids: number;
+//   taille: number;
+//   statistiques : [
+//     {
+//       base_stat: number;
+//       effort: number;
+
+//     },
+//     stat: {
+//       name: string;
+//       url: string;
+//     }
+//   ]
+//   capacites: any
+// }
+interface PokemonListItem {
+  name: string;
+  url: string;
+  id: number;
+}
+
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class PokemonService {
+  private http = inject(HttpClient);
+  private url = 'https://pokeapi.co/api/v2/pokemon?limit=50'; // URL api avec limite 50
 
-  constructor() { }
+  constructor() {}
+
+getPokemons(): Observable<PokemonListItem[]> {
+  return this.http.get<{ results: PokemonListItem[] }>(this.url).pipe(
+    map(response => response.results)
+  );
 }
+
+  // getPokemons(): Observable<
+  //   { name: string; url: string; id: number; imageUrl: string }[]
+  // > {
+  //   return this.http
+  //     .get<{ results: { name: string; url: string }[] }>(this.url)
+  //     .pipe(
+  //       map((response: { results: { name: string; url: string }[] }) => {
+  //         return response.results.map((pokemon) => {
+  //           const urlParts = pokemon.url.split('/');
+  //           const id = Number(urlParts[urlParts.length - 2]);
+
+  //           return {
+  //             name: pokemon.name,
+  //             url: pokemon.url,
+  //             id: id,
+  //             imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+  //           };
+  //         });
+  //       })
+  //     );
+  // }
+
+  getPokemonDetails(name: string) {}
+}
+
+/**map(response =>
+  response.results.map(pokemon => {
+    const id = ...;
+    const imageUrl = ...;
+
+    return {
+      name: ...,
+      id: ...,
+      imageUrl: ...
+    };
+  })
+)
+ */
+// bout de code pour récupérer les images
