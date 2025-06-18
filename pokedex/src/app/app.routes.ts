4f62973860ff6ec53226748369a122f0cc69b95c
Imane <barbecheimane@gmail.com>
2025-06-18 14:08:24 +0200

    composants enfants crées, app confi et routes configurés au minimum. Début du  service.

--- pokedex/src/app/app.routes.ts
+++ pokedex/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PokemonDetailComponent } from './components/pokemon-detail/pokemon-detail.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'detail', component: PokemonDetailComponent},
+    { path: 'list', component: PokemonListComponent},
+    { path: '**', redirectTo: '', pathMatch: 'full'}
+
+];
